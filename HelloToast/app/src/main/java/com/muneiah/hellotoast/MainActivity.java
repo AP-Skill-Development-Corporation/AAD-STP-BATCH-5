e1b661138cb3802e12c8359e5bf764ff40ace50d
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-07-15 12:52:48 +0530

    Day 3 Hello Toast

---
+++ HelloToast/app/src/main/java/com/muneiah/hellotoast/MainActivity.java
@@ -0,0 +1,21 @@
+package com.muneiah.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToastMessage(View view) {
+    }
+
+    public void increaseCount(View view) {
+    }
+}

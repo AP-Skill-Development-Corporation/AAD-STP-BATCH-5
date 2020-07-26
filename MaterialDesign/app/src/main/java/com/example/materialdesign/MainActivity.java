8415124a0d6edfd95c131221a46213a3567eb401
sai <saisankar75@gmail.com>
2020-07-26 11:32:43 +0530

    Material Design Updated

---
+++ MaterialDesign/app/src/main/java/com/example/materialdesign/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void materialdesign(View view) {
+        startActivity(new Intent(this,MyMaterialDesign.class));
+    }
+}
---
+++ MaterialDesign/app/src/main/java/com/example/materialdesign/MyMaterialDesign.java
@@ -0,0 +1,14 @@
+package com.example.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyMaterialDesign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_material_design);
+    }
+}

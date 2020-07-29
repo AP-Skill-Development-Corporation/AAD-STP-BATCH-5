a416a51e237e76682c9b0519017f5bf383b0893a
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-07-29 10:42:45 +0530

    Room Db Project CURD Opartions completed without live data added

--- RoomDbExample/app/src/main/java/com/muneiah/roomdbexample/StudentAdaper.java
+++ RoomDbExample/app/src/main/java/com/muneiah/roomdbexample/StudentAdaper.java
@@ -1,6 +1,7 @@
 package com.muneiah.roomdbexample;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,6 +36,12 @@ public class StudentAdaper extends RecyclerView.Adapter<StudentAdaper.StudentVie
     public void onBindViewHolder(@NonNull StudentViewHolder holder, int position) {
         holder.stud_name.setText(list.get(position).getName());
         holder.stu_rollnumber.setText(list.get(position).getRollnumber());
+        holder.stu_dele.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.database.studentDAO().delete(list.get(position));
+            }
+        });
     }
 
     @Override
@@ -43,12 +50,28 @@ public class StudentAdaper extends RecyclerView.Adapter<StudentAdaper.StudentVie
     }
 
     public class StudentViewHolder extends RecyclerView.ViewHolder {
-        TextView stud_name,stu_rollnumber;
+        TextView stud_name,stu_rollnumber,stu_edit,stu_dele;
 
         public StudentViewHolder(@NonNull View itemView) {
             super(itemView);
             stud_name=itemView.findViewById(R.id.tv_sName);
             stu_rollnumber=itemView.findViewById(R.id.tv_srollnumber);
+            stu_edit=itemView.findViewById(R.id.edit);
+            stu_dele=itemView.findViewById(R.id.delete);
+            stu_edit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String n=stud_name.getText().toString();
+                    String r=stu_rollnumber.getText().toString();
+                    Intent i=new Intent(ctx,UpdateActivity.class);
+                    i.putExtra("name",n);
+                    i.putExtra("roll",r);
+                   ctx.startActivity(i);
+
+
+                }
+            });
+
         }
     }
 }
---
+++ RoomDbExample/app/src/main/java/com/muneiah/roomdbexample/UpdateActivity.java
@@ -0,0 +1,40 @@
+package com.muneiah.roomdbexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class UpdateActivity extends AppCompatActivity {
+EditText editText_name,editText_rollnumber;
+Student_Entity studentEntity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+        editText_name=findViewById(R.id.student_name_update);
+        editText_rollnumber=findViewById(R.id.student_rollnumber_update);
+        Intent i=getIntent();
+        String sn=i.getStringExtra("name");
+        String sr=i.getStringExtra("roll");
+        editText_name.setText(sn);
+        editText_rollnumber.setText(sr);
+        editText_rollnumber.setKeyListener(null);//we don't modify the edittext field
+
+    }
+
+    public void updateData(View view) {
+        String myUpdatename=editText_name.getText().toString();
+        String myUpdaterollnum=editText_rollnumber.getText().toString();
+        studentEntity=new Student_Entity();
+        studentEntity.setName(myUpdatename);
+        studentEntity.setRollnumber(myUpdaterollnum);
+        MainActivity.database.studentDAO().update(studentEntity);
+        Toast.makeText(this, "updated"+myUpdatename, Toast.LENGTH_SHORT).show();
+    Intent intent=new Intent(this,MainActivity.class);
+    startActivity(intent);
+    }
+}
